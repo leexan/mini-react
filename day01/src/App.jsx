da95f27272e3927726384294aced5eaec9d958f9
lishuai <244821338@qq.com>
2024-01-13 10:53:56 +0800

    day01

---
+++ day01/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "./core/React.js";
+// const App = React.createElement('div',{id:'app'},'hi- ','mini-react')
+const App =<div>haha
+
+<span>heihei</span>
+</div>
+export default App
---
+++ main.js
@@ -0,0 +1,3 @@
+import ReactDom from './day01/src/core/ReactDom.js'
+import App from './day01/src/App.jsx'
+ReactDom.createRoot(document.querySelector('#root')).render(App)
